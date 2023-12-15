f342b4ab7ae7b137da848db331ce118671d68664
root <root@myfirstvm.us-central1-c.c.dulcet-theory-403115.internal>
2023-12-15 16:06:38 +0000

    chnages from gcp machine

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 // your first program old11
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World, Welcome Git");
+        System.out.println("Hello, World, Welcome Git.it is from clone" );
     }
 }
